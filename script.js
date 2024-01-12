2a833468afe105914bbb77b8df57aff8345c69ec
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-01-12 17:41:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function makeid(l) {
-  // write your code here
+  let words = ["a","b","c","d","e","f","g","h","i","j","k","l","m"]
+	let str = "";
+	for(let i=0;i<=l;i++){
+		str+= words[Math.floor(Math.random()*12)];
+		
+	}
+return str;
 }
 
 // Do not change the code below.
