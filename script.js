afa80a11e86ff9a9dcb8f95008bda06f959270aa
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-01-22 19:43:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 function makeid(l) {
-  let words = ["a","b","c","d","e","f","g","h","i","j","k","l","m"]
+	let arr = [
+  'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+  'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r',
+  's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A',
+  'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+  'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2',
+  '3', '4', '5', '6', '7', '8', '9'
+]
 	let str = "";
-	for(let i=0;i<=l;i++){
-		str+= words[Math.floor(Math.random()*12)];
-		
+	for(let i=0;i<=l-1;i++){
+		let num = parseInt(Math.random()*60);
+		str+= arr[num];
 	}
-return str;
+	return str;
 }
 
 // Do not change the code below.
